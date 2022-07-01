49c97cf70b711e3f4268b42737843ad46d18c9c3
guismeiram <boralagui@outlook.com>
2022-06-30 21:17:17 -0300

    commitando jpql agora foi

--- src/main/java/com/Entrevista/repository/MoedasRepository.java
+++ src/main/java/com/Entrevista/repository/MoedasRepository.java
@@ -4,13 +4,14 @@ import com.Entrevista.models.Moedas;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
 @Repository
 public interface MoedasRepository extends JpaRepository<Moedas, Long> {
 
-    @Query(value = "SELECT * FROM moedas m  WHERE m.rank < :rank  ORDER BY m.rank ASC Limit 50",
+    @Query(value = "SELECT * FROM MOEDAS LIMIT 50",
             nativeQuery = true)
-    List<Moedas> findTopRankMoedas(Long rank);
+    List<Moedas> findTopRankMoedas(@RequestParam(value = "rank") Long rank);
 }
